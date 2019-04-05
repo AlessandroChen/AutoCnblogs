a537c94560e8dad8fe33a845ab9db2b45f80efe3
AlexanderChen <alex_passenger@outlook.com>
2019-04-05 19:33:41 +0800

    Upload New Part: Create Config

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 config_path = "./blog_config.json"
 
-def have_configed():
+def have_config():
     '''
     return bool value : whether config file exists
     '''
@@ -11,7 +11,7 @@ def have_configed():
         with open(config_path, "r", encoding = "utf-8") as f:
             try:
                 cfg = json.load(f)
-                return cfg == {}
+                return cfg != {}
             except json.decoder.JSONDecodeError:
                 return False
     except:
@@ -19,6 +19,36 @@ def have_configed():
             json.dump({}, f)
             return False
 
+def create_config():
+    '''
+    create config file if it doesnt exist
+    only save ensured data usr provided, if not print ERROR
+    '''
+    # Provide EXAMPLE Here
+
+    for test_times in range(0, 2):
+        cfg = {}
+        for item in [("url", "metaWeblog url"),
+                     ("appkey", "blogaddress"),
+                     ("usr", "usrname"),
+                     ("passwd", "password")]:
+            cfg[item[0]] = input(item[1] + " : ")
+        
+        try:
+            server = xmlrpclib.ServerProxy(cfg["url"])
+            userInfo = server.blogger.getUsersBlogs(
+                    cfg["appkey"], cfg["usr"], cfg["passwd"])
+            print (userInfo[0])
+            cfg["bolgid"] = userInfo[0]["blogid"]
+            break
+        except:
+            print ("ERROR!!!")
+            print ("Please Check It Again")
+    with open(config_path, "w", encoding = "utf-8") as f:
+        json.dump(cfg, f, indent = 4, ensure_ascii = False)
+
+
 if __name__ == "__main__":
-    if have_configed() == False:
-        pass
+    if have_config() == False:
+        create_config()
+    print ("End")
